e408268e85ff7522ae98fb4a1c3902c7ec5a6157
Phil Plencner <philip_plencner@harvard.edu>
2022-03-14 10:34:22 -0400

    Adding hideViewerNavigation component.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import { getManifestoInstance } from 'mirador/dist/es/src/state/selectors/manifests';
 
-export default class extends Component {
+class hideViewerNavigation extends Component {
+
+  viewingHint() {
+    const { manifest } = this.props;
+    if (!(
+      manifest
+      && manifest.getSequences()
+      && manifest.getSequences()[0]
+      && manifest.getSequences()[0].getProperty('viewingHint')
+    )) return [''];
+
+    return manifest.getSequences()[0].getProperty('viewingHint');
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+
+    return (
+      <></>
+    );
+  }
+
+  componentDidUpdate() {
+    let viewingHint = this.viewingHint();
+
+    if (viewingHint == 'individuals') {
+      window.document.querySelectorAll('.mirador-osd-info').forEach((elem) => elem.remove());
+      window.document.querySelectorAll('.mirador-osd-navigation').forEach((elem) => elem.remove());
+      window.document.querySelectorAll('[class*="Connect(WithPlugins(ZoomControls))-divider-"]').forEach((elem) => elem.remove());
+    }
   }
 }
+
+const mapStateToProps = (state, { windowId }) => ({
+  manifestId: (getManifestoInstance(state, { windowId }) || {}).id,
+  manifest: getManifestoInstance(state, { windowId }),
+});
+
+export default {
+  target: 'Window',
+  mode: 'add',
+  component: hideViewerNavigation,
+  mapStateToProps,
+}
+
